80e972ff26faaae350675208a76adf8674c09fca
山本大志 <taishi020294@gmail.com>
2021-02-22 21:44:43 +0900

    LINEAPIの修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # ngrok を立ち上げた際に発行される URL のドメイン部分（https:// 以降)を以下のように記載する
+  config.hosts = ["5c8c094bb344.ngrok.io", "localhost"]
 end
