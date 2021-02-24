8911ec1339fa994766b6007ca1c7310601934f67
山本大志 <taishi020294@gmail.com>
2021-02-25 00:07:22 +0900

    Rspecの環境構築

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,9 @@ module LineTodoList
       g.javascripts false
       g.stylesheets false
       g.helper false
-      g.test_framework false
-      g.template_engine false
+      g.test_framework :rspec
     end
 
-    config.api_only = true
     config.middleware.use ActionDispatch::Flash
   end
 end
